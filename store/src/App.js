44aa0067bfea17a465d7664114b5000ae4a041f2
KusokKolbasi <tyomikfds@gmail.com>
2023-11-22 19:59:09 +0400

    Create placeholder header

--- store/src/App.js
+++ store/src/App.js
@@ -1,7 +1,27 @@
 export default function App() {
   return (
-    <div className="App">
-      
+    <div className="wrapper clear">
+      <header className="d-flex justify-between align-center">
+       <div className="d-flex align-center">
+        <img width={40} height={40} src="./misc/examplelogo.svg" alt="Logo"/>
+          <div>
+            <h3>Online Store</h3>
+            <p>Vinyl album store</p>
+          </div>
+       </div>
+          <ul className="d-flex align-center">
+            <li className="mr-10">
+              <img src="./misc/cart1.svg" alt="Cart"/>
+              <span className="ml-10">Money amount</span>
+            </li>
+            <li>
+              <img src="./misc/user1.svg" alt="User"/>
+            </li>
+          </ul>
+      </header>
+      <div className="content">
+        <h1>Best-Sellers</h1>
+      </div>
     </div>
   );
 }
