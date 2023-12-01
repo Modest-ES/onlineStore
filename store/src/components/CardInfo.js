1a166f00ca0ea59543b0cf3a9c3a836ae03f6024
KusokKolbasi <tyomikfds@gmail.com>
2023-12-01 19:21:28 +0400

    CardInfoChanged

--- store/src/components/CardInfo.js
+++ store/src/components/CardInfo.js
@@ -1,54 +1,28 @@
-export default function CardInfo({onClickBtnBack}) {
+export default function CardInfo({albumCover, albumName, albumPrice, albumDescription, onClickBtnBack, onClickPlus, onClickFavorite}) {
       return(
-          <div className="cart-overlay-shell">
-            <div className="cart-overlay-shadow" onClick={onClickBtnBack}>
+          <div className="card-info-overlay-shell">
+            <div className="card-info-overlay-shadow" onClick={onClickBtnBack}>
             </div>
-          <div className="cart-overlay">
-              <div className="cart-title">
-                <div className="d-flex align-center">
-                  <button className="btn-back" onClick={onClickBtnBack}>
-                    <img width={32} height={32} src="./misc/btn-back.png" alt="Вернуться" title="Вернуться" />
-                  </button>
-                  <h3>Корзина</h3>
-                </div>
-                <button className="btn-clear-cart">
-                  <h4>Очистить</h4>
-                </button>
+          <div className="card-info-overlay">
+            <div className="card-info-left-side">
+              <div className="card-info-image">
+                <img src={albumCover} alt={albumName} title={albumName} />
               </div>
-              <div className="cart-content">
-                {
-                  items.map((item) => (
-                    <div className="cart-item">
-                      <div className="cart-item-info">
-                        <img src={item.albumCover} alt={item.albumName} title={item.albumName} className="cart-item-cover" />
-                        <div>
-                          <h4>{item.albumName}</h4>
-                          <b>${item.albumPrice}</b>
-                        </div>
-                      </div>
-                      <button className="cart-btn-delete">
-                        <img src="./misc/btn-delete.png" alt="Удалить товар" title="Удалить товар" />
-                      </button>
-                    </div>
-                  ))
-                }
-  
-              </div>
-            <div className="cart-bottom">
-              <div className="cart-total">
-                <b>Итого:</b>
-                <div className="dashline"></div>
-                <b>Сумма</b>
-              </div>
-              <div className="cart-total">
-                <b>НДС (13%):</b>
-                <div className="dashline"></div>
-                <b>Сумма</b>
-              </div>
-              <button className="cart-btn-order">
-                <h2>Оформить заказ</h2>
+              <h3>{albumName}</h3>
+              <h4>{albumPrice}</h4>
+            </div>
+            <div className="card-info-right-side">
+              <h4>Описание:</h4>
+              <p>{albumDescription}</p>
+              <button className="d-flex align-center" onClick={onClickFavorite}>
+                <img width={16} height={16} src="./misc/btn-heart-empty.png" alt="Добавить в избранное" title="Добавить в избранное" />
+                Добавить в избранное
               </button>
-            </div>
+              <button className="d-flex align-center" onClick={onClickPlus}>
+                <img width={16} height={16} src="./misc/btn-plus.png" alt="Добавить в корзину" title="Добавить в корзину" />
+                Добавить в корзину
+              </button>
+            </div>              
           </div>
         </div>
       );
